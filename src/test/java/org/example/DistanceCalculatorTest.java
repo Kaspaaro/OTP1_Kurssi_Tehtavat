2a00e8ad12ed9e1da66cf41b512eef6a2d9a1c8a
KasparT <kaspar.tullusgmail.com>
2023-09-19 19:54:16 +0300

    fix

--- src/test/java/org/example/DistanceCalculatorTest.java
+++ src/test/java/org/example/DistanceCalculatorTest.java
@@ -5,12 +5,13 @@ package org.example;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
 class DistanceCalculatorTest {
     @Test
     public void calculateDistance() {
         double result = DistanceCalculator.calculateDistance(2.0,2.0,8.0,10.0);
-        double DELTA = 0.00001;
+        double DELTA = 0;
         assertEquals(10.0, result,DELTA, "Distance Between Two Points is 10");
     }
 }
