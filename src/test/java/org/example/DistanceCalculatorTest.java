c285d9fc3d7d908ee1abbf24bd666f7380406a26
KasparT <kaspar.tullusgmail.com>
2023-09-19 18:25:44 +0300

    fix

---
+++ src/test/java/org/example/DistanceCalculatorTest.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DistanceCalculatorTest {
+
+    @org.junit.jupiter.api.Test
+    void calculateDistance() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void main() {
+    }
+}
